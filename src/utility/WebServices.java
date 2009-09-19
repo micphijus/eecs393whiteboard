5b684b6ac3e9e37fd64f2648b061c263f7fc5195
Felix <fxy14@case.edu>
2009-09-19 13:51:03 -0400

    Added register method.  Uses md5 hashing.  Assumes name of xmlrpc method will be "Register"

--- src/utility/WebServices.java
+++ src/utility/WebServices.java
@@ -1,10 +1,13 @@
 package utility;
 
+import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Vector;
 
 //This class is actually several utility methods associated with XMLRPC
@@ -12,8 +15,11 @@ import java.util.Vector;
 //xmlrpc webservice
 
 public class WebServices {
+	
 	XmlRpcClient client;
 	XmlRpcClientConfigImpl config;
+	MessageDigest md;
+	
 	String url;
 	
 	//initialize the webservice stuff
@@ -22,6 +28,7 @@ public class WebServices {
 	{
 		client = new XmlRpcClient();
 		config = new XmlRpcClientConfigImpl();
+		
 	}
 	
 	//overloaded constructor
@@ -39,10 +46,35 @@ public class WebServices {
 		{
 			config.setServerURL(new URL(url));
 			client.setConfig(config);
+			md = MessageDigest.getInstance("MD5");
 		}
 		catch(MalformedURLException e)
 		{
-			System.out.println("Bad URL");
+			System.out.println("Bad URL: " + e);
+		}
+		catch(NoSuchAlgorithmException e)
+		{
+			System.out.println("No such algorithm: " + e);
+		}
+	}
+	
+	public void Register(String name, String username, String email, String password)
+	{
+		md.update(password.getBytes());
+		String md5password = md.digest().toString();
+		md.reset();
+		Vector<String> params = new Vector<String>();
+		params.add(name); 
+		params.add(email);
+		params.add(username);
+		params.add(md5password);
+		try
+		{
+			client.execute("Register", params);
+		}
+		catch(XmlRpcException e)
+		{
+			System.out.println("Caught XMLRPCException: " + e);
 		}
 	}
 
