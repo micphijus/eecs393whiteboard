7ea176dc60d8f0ff76a46a5f77a296771a9497dc
Felix <fxy14@case.edu>
2009-10-03 18:40:02 -0400

    Changed class path about.  Added Smack.

--- src/core/utility/Registration.java
+++ src/core/utility/Registration.java
@@ -1,5 +1,7 @@
 package core.utility;
 
+import java.util.Scanner;
+
 import org.jivesoftware.smack.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -7,12 +9,16 @@ import java.security.NoSuchAlgorithmException;
 public class Registration {
 	public static void main(String []args)
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter username: ");
+		String username = input.nextLine();
+		System.out.print("Enter password: ");
+		String password = input.nextLine();
 		try
 		{
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			XMPPConnection conn = new XMPPConnection("127.0.0.1");
 			conn.connect();
-			
 		}
 		catch (NoSuchAlgorithmException e)
 		{
