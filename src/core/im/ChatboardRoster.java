b8d2ad21aa6b6670de3a7fd59255da8a426d86f1
Felix <fxy14@case.edu>
2009-10-13 00:46:30 -0400

    Added Chatboard message and fixed some Roster issues

--- src/core/im/ChatboardRoster.java
+++ src/core/im/ChatboardRoster.java
@@ -1,35 +1,43 @@
 package core.im;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Vector;
 
 import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.RosterListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.packet.Presence.Type;
 
 public class ChatboardRoster implements RosterListener{
 	XMPPConnection conn;
+	Vector<RosterEntry>online;
+	Vector<RosterEntry>offline;
 	Roster roster;
 	
 	public ChatboardRoster()
 	{
 		conn = null;
 		roster = null;
+		online = new Vector<RosterEntry>();
+		offline = new Vector<RosterEntry>();
 	}
 	
 	public ChatboardRoster(XMPPConnection conn)
 	{
+		super();
 		this.conn = conn;
-		roster = null;
 	}
 	
 	
-	public Roster pullRoster()
+	public void pullRoster()
 	{
 		roster = conn.getRoster();
 		roster.addRosterListener(this);
-		return roster;
+		updateOnline();
 	}
 	
 	public boolean updateRoster(String username, String alias, String group)
@@ -68,28 +76,59 @@ public class ChatboardRoster implements RosterListener{
 			System.out.println("Group already exists");
 		return false;
 	}
+	
+	public void updateOnline()
+	{
+		Collection<RosterEntry> rosterList = roster.getEntries();
+		Iterator<RosterEntry> iter = rosterList.iterator();
+		while(iter.hasNext())
+		{
+			RosterEntry entry = iter.next();
+			if(roster.getPresence(entry.getName()).getType() == Type.available)
+				online.add(entry);
+			else
+				offline.add(entry);
+		}
+	}
 
 	@Override
 	public void entriesAdded(Collection<String> arg0) {
 		// TODO Auto-generated method stub
+		pullRoster();
 		
 	}
 
 	@Override
 	public void entriesDeleted(Collection<String> arg0) {
-		// TODO Auto-generated method stub
+		pullRoster();
 		
 	}
 
 	@Override
 	public void entriesUpdated(Collection<String> arg0) {
-		// TODO Auto-generated method stub
+		pullRoster();
 		
 	}
 
 	@Override
 	public void presenceChanged(Presence arg0) {
-		// TODO Auto-generated method stub
+		//First get the user
+		//then get the entry
+		//Finally remove the entry from one list, and add to the other
+		String user = arg0.getFrom();
+		RosterEntry entry = roster.getEntry(user);
+		if(arg0.getType() == Type.available)
+		{
+			if(offline.contains(entry))
+				offline.remove(entry);
+			online.add(entry);
+		}
+		else
+		{
+			if(online.contains(entry))
+				online.remove(entry);
+			offline.add(entry);
+		}
 		
 	}
 
