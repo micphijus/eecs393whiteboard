a416377409495664d7032a8372bbbdd33d32d338
Justin <micphijus@yahoo.com>
2009-11-01 15:19:35 -0500

    committing for felix

--- src/gui/MessageDialog.java
+++ src/gui/MessageDialog.java
@@ -5,8 +5,11 @@ import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 public class MessageDialog {
 
@@ -38,9 +41,19 @@ public class MessageDialog {
 		conversation.setLayout(bl); 
 		//TODO: for adding a menu later
 	//	conversation.setJMenuBar(menu);
-		JPanel convoWindow = new JPanel();
-		convoWindow.setPreferredSize(new Dimension(300, 200));
-		conversation.add(convoWindow);
+		JPanel convoPanel = new JPanel();
+		convoPanel.setPreferredSize(new Dimension(300, 200));
+		JScrollPane scrollWindow = new JScrollPane();
+		scrollWindow.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollWindow.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		JEditorPane convoWindow = new JEditorPane();
+		convoWindow.setContentType("text/plain");
+		convoWindow.setText("The first test!");
+		//TODO: Will need a method that keeps a large string of the conversation and inserts usernames and shit
+		
+		scrollWindow.add(convoWindow);
+		convoPanel.add(scrollWindow);
+		conversation.getContentPane().add(convoPanel);
 		JTextField inputField = new JTextField();
 		inputField.setPreferredSize(new Dimension(300, 150));
 		conversation.add(inputField);
